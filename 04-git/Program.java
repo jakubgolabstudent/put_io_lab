4aa156228c04ff8bb91fb7ae17854deb20438fd6
unknown <student@lab-44-3.cs.put.poznan.pl>
2022-10-27 14:00:59 +0200

    Git staged

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,7 +1,9 @@
+/* Hello World Class */
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World! :-)"); 
     }
 }
 /* My comment :) */
+/* Second comment */
 
